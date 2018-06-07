046a47749df51664b4eb3065c1a7f9bbf8f39dac
Varun <varunvenkatasaiteja@gmail.com>
2018-06-07 10:23:50 +0530

    Renamed form1.vb to checker.vb

--- Checker/My Project/Application.Designer.vb
+++ Checker/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Checker.Form1
+            Me.MainForm = Global.Checker.Tool
         End Sub
     End Class
 End Namespace
